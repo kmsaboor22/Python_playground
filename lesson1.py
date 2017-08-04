542da825305d83733c60f86b34072cca7c58258b
Khalil Saboor <themaninthefoxhole@gmail.com>
2017-08-04 16:03:30 -0400

    doc

--- lesson1.py
+++ lesson1.py
@@ -1,6 +1,7 @@
-
-#Ask the user to input their name and assign it to a variable name.
+#Ask the user to input their name and assign it to a variable name
 
 username = input('What is your name ')
+
+#Print out hello followed by the name they enter
 
 print('Hello', username)
