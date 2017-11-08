5cd21d9eb9d2c73d0569b7db5e2ebd9696179afe
Khalil Saboor <themaninthefoxhole@gmail.com>
2017-11-07 22:23:00 -0500

    comments and list

--- comments.py
+++ comments.py
@@ -1,8 +1,8 @@
-#the goal of this program is to find the magicNumber.
+# the goal of this program is to find the magicNumber.
 
 magicNumber = 27
 
-#To print differnt data types on the same line NOTE: remove # on second line to run code.
+# To print differnt data types on the same line NOTE: remove # on second line to run code.
 # print("AK",47)
 
 ''' This is a multiline comment
@@ -10,8 +10,11 @@ for n in range(101):
     if n is magicNumber:
         print()
 '''
-#To print the same data types on the same line
+# To print the same data types on the same line
 # print("Hello my name is " + "Khalil")
 for n in range(101):
     if n is magicNumber:
         print(n, "Is the magic number!")
+        break
+    else:
+        print(n)
