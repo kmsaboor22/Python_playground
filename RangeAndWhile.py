e30f4ce54aae5af77e2ee6bad0b8bdb764c59d89
Khalil Saboor <themaninthefoxhole@gmail.com>
2017-11-11 22:58:45 -0500

    Ranges

---
+++ RangeAndWhile.py
@@ -0,0 +1,31 @@
+
+'''
+Print 0 to 9
+'''
+
+'''for x in range(10):
+    print(x)
+'''
+
+'''
+Print 5 to 11
+'''
+
+'''for x in range(5,12):
+    print(x)
+'''
+
+'''
+2, Start printing at 2
+20, Print up to 20th value
+2, Incurment printing by 2
+'''
+'''for x in range(2,20,2):
+    print(x)
+'''
+#While - loops as long the cont is True
+
+odd = 5
+while odd < 10:
+    print(odd)
+    odd += 1
